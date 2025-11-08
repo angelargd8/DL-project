2162aa69eaf4e54f61ad869fabfc990e2b9bc712
Angela Garcia <100092195+angelargd8@users.noreply.github.com>
2025-11-07 21:57:24 -0600

    feat: add model

---
+++ Assets/EmojiRecognition.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Sentis;
+
+public class EmojiRecognition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //run the model
+    public void runAI()
+    {
+
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+}
