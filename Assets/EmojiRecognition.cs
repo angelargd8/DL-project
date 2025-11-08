17eb055714ccd166ab626a7e2d2550690f28e3f1
Angela Garcia <100092195+angelargd8@users.noreply.github.com>
2025-11-07 22:04:36 -0600

    chore: init IA

--- Assets/EmojiRecognition.cs
+++ Assets/EmojiRecognition.cs
@@ -5,10 +5,25 @@ using Unity.Sentis;
 
 public class EmojiRecognition : MonoBehaviour
 {
+    public ModelAsset modelAsset;
+
+    private Worker worker;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // init IA
+        Model model = ModelLoader.Load(modelAsset);
+
+        //graph 
+        FunctionalGraph graph = new FunctionalGraph();
+        FunctionalTensor[] inputs = graph.AddInputs(model);
+        FunctionalTensor[] outputs = Functional.Forward(model, inputs);
+
+        Model runtimeModel = graph.Compile(outputs);
+        worker = new Worker(runtimeModel, BackendType.GPUCompute);
+
+
     }
 
     //run the model
